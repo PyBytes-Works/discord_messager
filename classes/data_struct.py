de54f7b25b2e481ab1176d00331d05a8a0394e48
Владимр Леонтьев <dllreg.exe@gmail.com>
2022-04-26 23:23:22 +0800

    Fix data_struct.py Menu

--- classes/data_struct.py
+++ classes/data_struct.py
@@ -1,4 +1,4 @@
 class Menu:
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
-            getattr(self, str(key), str(value))
+            setattr(self, str(key), str(value))
