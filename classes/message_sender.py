cca594a1166ab3f945c5f57db86bea88e5751394
deskent <battenetciz@gmail.com>
2022-04-28 10:01:22 +0300

    version 4.8.4 add handling for 429 error code 40062

--- classes/message_sender.py
+++ classes/message_sender.py
@@ -64,7 +64,8 @@ class MessageSender(PostRequest):
                      f"\nData for send: {self._data_for_send}")
         result: dict = await ErrorsSender(**self._error_params).handle_errors()
         data: dict = result.get('answer_data', {})
-        if status == 429:
+        code: int = data.get("code")
+        if status == 429 and code == 20016:
             cooldown: int = int(data.get("retry_after"))
             if cooldown:
                 cooldown += self._datastore.cooldown
