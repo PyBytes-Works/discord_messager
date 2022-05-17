a463bfc4e1c74457d002b778a523e09d81666ee1
deskent <battenetciz@gmail.com>
2022-05-17 21:19:47 +0300

    version 4.11.34 add test for GetRequest

---
+++ tests/test_get_request_class.py
@@ -0,0 +1,24 @@
+from classes.request_classes import GetRequest
+
+
+async def test_get_request():
+    url = 'http://superpomerashki.xyz/scripts/my_ip'
+    spam = GetRequest()
+    spam._params = {
+        'url': url,
+    }
+    answer: dict = await spam._send()
+    assert answer.get("status") == 200
+
+
+async def test_get_request_wrong_url():
+    url = 'http://superpomerashki.xyz/scripts/my_i'
+    spam = GetRequest()
+    spam._params = {
+        'url': url,
+    }
+    answer: dict = await spam._send()
+    assert answer.get("status") == 404
+
+
+# TODO написать тесты для овердофига запросов и для PostRequest
