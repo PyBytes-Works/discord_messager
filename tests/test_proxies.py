aec4d4fc6789afc8d5df16e91a9f1d4344d8995d
deskent <battenetciz@gmail.com>
2022-05-18 22:30:30 +0300

    version 4.11.52 add start.sh

--- tests/test_proxies.py
+++ tests/test_proxies.py
@@ -5,10 +5,19 @@ from fake_data import proxies
 from config import PROXY_USER, PROXY_PASSWORD
 
 
-@pytest.mark.parametrize("sent, received", [items for items in zip(proxies, proxies)])
+@pytest.mark.parametrize("sent, received", [(sent, received[:-6]) for sent, received in zip(proxies, proxies)])
 async def test_send_request(sent, received):
     async with aiohttp.ClientSession() as session:
         url = 'http://superpomerashki.xyz/scripts/my_ip'
         proxy = f"http://{PROXY_USER}:{PROXY_PASSWORD}@{sent}/"
         async with session.get(url=url, proxy=proxy) as response:
-            assert await response.text() == received[:-6]
+            assert await response.text() == received
+
+
+@pytest.mark.parametrize("sent, received", [("185.240.120.61:45785", "185.240.120.61")])
+async def test_one_proxy(sent, received):
+    async with aiohttp.ClientSession() as session:
+        url = 'http://superpomerashki.xyz/scripts/my_ip'
+        proxy = f"http://{PROXY_USER}:{PROXY_PASSWORD}@{sent}/"
+        async with session.get(url=url, proxy=proxy) as response:
+            assert await response.text() == received
