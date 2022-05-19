14738bf99effb1543252faaa793c6a63e40e4347
deskent <battenetciz@gmail.com>
2022-05-19 09:56:26 +0300

    version 4.11.56 del semaphore from redis_interface.py decrease redis timeout to 300

--- classes/redis_interface.py
+++ classes/redis_interface.py
@@ -3,7 +3,7 @@ import json
 import aioredis
 from typing import List
 
-from config import logger, REDIS_DB, SEMAPHORE
+from config import logger, REDIS_DB
 
 
 class RedisDB:
@@ -13,8 +13,7 @@ class RedisDB:
         self.redis_db = aioredis.from_url(url=REDIS_DB, encoding="utf-8", decode_responses=True)
         self.redis_key: str = redis_key
         self.data: list = []
-        self.timeout_sec: int = 600
-        self.semaphore = SEMAPHORE
+        self.timeout_sec: int = 300
 
     async def _send_request_do_redis_db(self, key: str, mate_id: str = '', data: list = None) -> list:
         """Запрашивает или записывает данные в редис, возвращает список если запрашивали"""
@@ -68,19 +67,16 @@ class RedisDB:
         self.data: list = data
         if timeout_sec:
             self.timeout_sec: int = timeout_sec
-        async with self.semaphore:
-            await self._send_request_do_redis_db(key="set")
+        await self._send_request_do_redis_db(key="set")
 
     @logger.catch
     async def load(self) -> list:
         """Возвращает десериализованные данные из Редис (список)"""
 
-        async with self.semaphore:
-            return await self._send_request_do_redis_db(key="get")
+        return await self._send_request_do_redis_db(key="get")
 
     @logger.catch
     async def delete(self, mate_id: str) -> List[dict]:
         """Удаляет данные из Редис для себя и напарника"""
 
-        async with self.semaphore:
-            return await self._send_request_do_redis_db(key="del", mate_id=mate_id)
+        return await self._send_request_do_redis_db(key="del", mate_id=mate_id)
